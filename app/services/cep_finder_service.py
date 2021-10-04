6ff3ef621ab3de835c6d550e05f2c49ed4a32397
douglas.ronchi <douglas.ronchi@ambevtech.com.br>
2021-10-04 16:35:16 -0300

    Initial Commit

--- app/services/cep_finder_service.py
+++ app/services/cep_finder_service.py
@@ -1,14 +1,8 @@
 import re
-from multiprocessing import Process
-from multiprocessing.pool import ThreadPool
 
 from app.exceptions.cep_finder_exceptions import CepFinderError, AllServicesNotFound
 from app.models.address_information_model import AddressInformation
-from app.services.cep_services.brasilapi import fetch_brasilapi_service
-from app.services.cep_services.correios import fetch_correios_service
 from app.services.cep_services.index import get_available_services
-from app.services.cep_services.viacep import fetch_via_cep_service
-from app.services.cep_services.widenet import fetch_wide_net_service
 
 CEP_SIZE = 8
 
@@ -57,13 +51,6 @@ class CepService:
         cep_number = self.__cep_validator(cep_number)
         providers_services = get_available_services()
         # Search in available services:
-        # pool = ThreadPool(processes=4)
-        # async_result = pool.apply_async(fetch_brasilapi_service, (cep_number))
-        # async_result = pool.apply_async(fetch_correios_service, (cep_number))
-        # async_result = pool.apply_async(fetch_via_cep_service, (cep_number))
-        # async_result = pool.apply_async(fetch_wide_net_service, (cep_number))
-        # return async_result.get()
-
         for provider in providers_services:
             try:
                 response = provider(cep_number)
